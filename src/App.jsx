e159574121dce252615ea5e34399cf3625fe9a5e
Michael Asiedu <michaelasiedu148@gmail.com>
2022-08-11 23:46:23 +0000

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+/**
+    * @description      : 
+    * @author           : Michael
+    * @group            : 
+    * @created          : 11/08/2022 - 23:40:34
+    * 
+    * MODIFICATION LOG
+    * - Version         : 1.0.0
+    * - Date            : 11/08/2022
+    * - Author          : Michael
+    * - Modification    : 
+**/
+
+
+function App() {
+
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
+}
+
+export default App
