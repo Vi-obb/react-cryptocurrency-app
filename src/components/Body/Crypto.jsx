1a4df83e55cdd1a56d9197eb283b309d10f7f7c3
Michael Asiedu <michaelasiedu148@gmail.com>
2022-08-12 00:32:30 +0000

    editted cryto.jsx file

--- src/components/Body/Crypto.jsx
+++ src/components/Body/Crypto.jsx
@@ -14,7 +14,7 @@ import React from 'react'
 
 const Crypto = () => {
 	return (
-		<div className='text-yellow-500'>Crypto</div>
+		<div className='text-yellow-500'>Cryptocurreny Component</div>
 	)
 }
 
