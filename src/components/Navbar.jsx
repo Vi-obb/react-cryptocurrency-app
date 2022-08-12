114c303fbd493cd67e50228042241ef30b7db61f
Michael Asiedu <michaelasiedu148@gmail.com>
2022-08-12 18:30:12 +0000

    new feature: navbar done!

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,15 +11,47 @@
     * - Modification    : 
 **/
 import React from 'react'
+import { FiChevronDown } from "react-icons/fi"
+import { RiCopyrightLine } from 'react-icons/ri'
 
 const style = {
-	wrapper: `text-cabaret font-karla`,
+	wrapper: `w-full flex flex-wrap text-sm justify-between items-center p-8`,
+	navLogo: `flex font-black`,
+	navItems: `flex justify-evenly items-center`,
+	navItem: `ml-3 mr-3 flex font-bold text-gray-700 cursor-pointer`,
+	navButton: `py-2 px-4 text-xs  bg-gray-700 focus:ring-white-500 focus:ring-offset-indigo-100 text-white w-full transition ease-in duration-200 text-center shadow-md focus:outline-none focus:ring-1 focus:ring-offset-2 `
 }
 
 const Navbar = () => {
 	return (
-		<div className={style.wrapper}>
-			Asiedu</div>
+		<nav className={style.wrapper}>
+			<div className={style.navLogo}>
+				<RiCopyrightLine class="text-xl mr-0.5" />
+				<p className='text-gray-800 '> Coin  </p>
+				<p className='text-cabaret'> X </p>
+			</div>
+			<div className={style.navItems}>
+				<div className={style.navItem}>
+					<p className='mr-0.5'> Market</p>
+					<FiChevronDown className="text-xl" />
+
+				</div>
+				<div className={style.navItem}>
+					<p>Community</p>
+				</div>
+				<div className={style.navItem}>
+					<p> Blog</p>
+				</div>
+			</div>
+			<div>
+
+				<button type="button" class={style.navButton}>
+					Log In
+				</button>
+
+			</div>
+
+		</nav>
 	)
 }
 
