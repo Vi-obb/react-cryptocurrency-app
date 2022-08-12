d69f1c4404bc098b435720c9da93b0a849dd93cd
Michael Asiedu <michaelasiedu148@gmail.com>
2022-08-12 22:16:43 +0000

    added text graident to navlogo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,7 @@ import { RiCopyrightLine } from 'react-icons/ri'
 
 const style = {
 	wrapper: `w-full flex flex-wrap text-sm justify-between items-center p-8`,
-	navLogo: `flex font-black`,
+	navLogo: `flex`,
 	navItems: `flex justify-evenly items-center`,
 	navItem: `ml-3 mr-3 flex font-bold text-gray-700 cursor-pointer`,
 	navBtnA: `py-2 px-4 text-xs  bg-gray-700 focus:ring-white-500 focus:ring-offset-indigo-100 text-white w-full transition ease-in duration-200 text-center shadow-md focus:outline-none focus:ring-1 focus:ring-offset-2 `
@@ -26,9 +26,9 @@ const Navbar = () => {
 	return (
 		<nav className={style.wrapper}>
 			<div className={style.navLogo}>
-				<RiCopyrightLine class="text-xl mr-0.5" />
-				<p className='text-gray-800 '> Coin  </p>
-				<p className='text-cabaret'> X </p>
+				<RiCopyrightLine class="gradient text-xl mr-0.5" />
+				<p className='text-gray-800 font-black '> Coin  </p>
+				<p className='gradient text-cabaret'> X </p>
 			</div>
 			<div className={style.navItems}>
 				<div className={style.navItem}>
