014a521bfaa77d8e73ed146e2331e806efc42421
Michael Asiedu <michaelasiedu148@gmail.com>
2022-08-16 19:01:22 +0000

    added price column to rightSplit side

--- src/components/RightSide.jsx
+++ src/components/RightSide.jsx
@@ -18,7 +18,8 @@ import { HiMenuAlt1 } from "react-icons/hi"
 const style = {
 	wrapper: `bg-gradient w-full flex flex-wrap text-sm justify-between items-center p-8`,
 	menu: `text-white text-2xl`,
-	menuDiv: `flex justify-end w-full`,
+	menuDiv: `flex justify-end w-full mb-5`,
+	priceDiv: `text-white`
 }
 const RightHandSide = () => {
 	return (
@@ -26,10 +27,10 @@ const RightHandSide = () => {
 			<div className={style.menuDiv}>
 				<HiMenuAlt1 className={style.menu} />
 			</div>
-			<div>
-				<h2></h2>
-				<h3></h3>
-				<p>Next 24h volume</p>
+			<div className={style.priceDiv}>
+				<p className='font-bold tracking-wider text-lg'>$50,134,689,775</p>
+				<p className='font-bold tracking-wider text-sm mb-3'> BTC 1,507,856 </p>
+				<p className='tracking-wide text-xs text-gray-100'>Next 24h volume</p>
 			</div>
 		</div>
 	)
