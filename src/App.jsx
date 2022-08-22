c5d5ab85fa56c1d7f2831e13ba0a098704dc327d
Michael Asiedu <michaelasiedu148@gmail.com>
2022-08-22 17:36:14 +0000

    footer spread

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,39 @@
 /**
-    * @description      : 
-    * @author           : Michael
-    * @group            : 
-    * @created          : 11/08/2022 - 23:40:34
-    * 
-    * MODIFICATION LOG
-    * - Version         : 1.0.0
-    * - Date            : 11/08/2022
-    * - Author          : Michael
-    * - Modification    : 
-**/
-import Headline from "./components/Body/Headline"
-import SplitScreen from "./components/SplitScreen"
-import Footer from "./components/Footer"
-import Navbar from "./components/Navbar"
-import RightHandSide from "./components/RightSide"
-import Content from "./components/Body/Content"
-
-
+ * @description      :
+ * @author           : Michael
+ * @group            :
+ * @created          : 11/08/2022 - 23:40:34
+ *
+ * MODIFICATION LOG
+ * - Version         : 1.0.0
+ * - Date            : 11/08/2022
+ * - Author          : Michael
+ * - Modification    :
+ **/
+import Headline from "./components/Body/Headline";
+import SplitScreen from "./components/SplitScreen";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import RightHandSide from "./components/RightSide";
+import Content from "./components/Body/Content";
 
 const LeftHandSide = () => {
-
   return (
     <>
       <Navbar />
       <Headline />
       <Content />
-      <Footer />
     </>
-  )
-}
-
-
+  );
+};
 
 function App() {
-
-
   return (
-
     <>
       <SplitScreen left={LeftHandSide} right={RightHandSide} />
+      <Footer />
     </>
-
-
-  )
+  );
 }
 
-export default App
+export default App;
