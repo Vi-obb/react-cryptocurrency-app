730c14f3e3a3769f9f0c81486d13888dbdefa6a8
7heMagician <obbengvincent@gmail.com>
2022-08-22 19:50:12 +0200

    updated styles in DisplayBanner component

--- src/components/DisplayBanner.jsx
+++ src/components/DisplayBanner.jsx
@@ -1,26 +1,22 @@
 import React from "react";
-import { HiMenuAlt1 } from "react-icons/hi";
 
 const style = {
-  wrapper: `bg-gradient w-full flex flex-wrap text-sm justify-between items-center p-8`,
+  wrapper: `bg-gradient w-full flex flex-wrap text-sm justify-between items-center p-5 mt-10`,
   menu: `text-white text-2xl`,
   menuDiv: `flex justify-end w-full mb-3`,
-  priceDiv: `text-white`,
+  priceDiv: `font-semibold tracking-wider font-poppins text-lg`,
 };
 
 const DisplayBanner = ({ active, ongoing, markets }) => {
   return (
     <div className={style.wrapper}>
-      <div className={style.menuDiv}>
-        <HiMenuAlt1 className={style.menu} />
-      </div>
-      <div className={style.priceDiv}>
-        <p className="font-bold tracking-wider text-lg">{markets} markets</p>
-        <p className="font-bold tracking-wider text-sm mb-3">
+      <div className="text-white">
+        <p className={style.priceDiv}>{markets} markets</p>
+        <p className={style.priceDiv}>
           {" "}
-          ICOs: {ongoing}
+          {ongoing} ICOs
         </p>
-        <p className="tracking-wide text-xs text-gray-100">
+        <p className={style.priceDiv}>
           {active} Cryptocurrencies
         </p>
       </div>
--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 
+const style = {
+  wrapper: `bg-gradient w-full flex flex-wrap text-sm justify-between items-center p-8 mt-10`,
+  heading: `mb-2 text-2xl font-bold tracking-tight text-white font-poppins`,
+  preview: `mb-3 font-normal font-karla text-white`,
+};
+
 const News = () => {
   return (
     <>
-      <div class="p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-        <a href="#">
-          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+      <div className={style.wrapper}>
+          <h5 className={style.heading}>
             Noteworthy technology acquisitions 2021
           </h5>
-        </a>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+        <p className={style.preview}>
           Here are the biggest enterprise technology acquisitions of 2021 so
           far, in reverse chronological order.
         </p>
