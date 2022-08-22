5c4d1a189628a4b5c454def47cc63c35ea70e095
7heMagician <obbengvincent@gmail.com>
2022-08-22 20:29:19 +0200

    created hook for fetching news

---
+++ hooks/useFetchNews.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const useFetchNews = () => {
+    const [news, setNews] = useState([]);
+
+    useEffect(() => { 
+        const results = {
+            method: 'GET',
+            url: 'https://crypto-news-live3.p.rapidapi.com/news',
+            headers: {
+                'X-RapidAPI-Key': 'e23fcb3be8msh1c0ed783cf62ab4p1003a0jsn6f142ec40a21',
+                'X-RapidAPI-Host': 'crypto-news-live3.p.rapidapi.com'
+            }
+        };
+
+        axios.request(results).then(function (response) {
+            setNews(response.data);
+            console.log(response.data);
+        }).catch(function (error) {
+            console.error(error);
+        });
+    }, []);
+
+    return {
+        news
+    }
+};
+
+export default useFetchNews;
