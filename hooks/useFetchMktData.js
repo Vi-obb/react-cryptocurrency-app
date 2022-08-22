972ba1c9d5fede850a22af996c70f82e6dc853ed
Michael Asiedu <michaelasiedu148@gmail.com>
2022-08-22 17:30:39 +0000

    customs hooks for mkt data

---
+++ hooks/useFetchMktData.js
@@ -0,0 +1,45 @@
+/**
+ * @description      :
+ * @author           : Michael
+ * @group            :
+ * @created          : 22/08/2022 - 17:27:09
+ *
+ * MODIFICATION LOG
+ * - Version         : 1.0.0
+ * - Date            : 22/08/2022
+ * - Author          : Michael
+ * - Modification    :
+ **/
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const useFetchMktData = () => {
+  // fetch data for banner
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const results = {
+      method: "GET",
+      url: "https://coingecko.p.rapidapi.com/global",
+      headers: {
+        "X-RapidAPI-Key": "e23fcb3be8msh1c0ed783cf62ab4p1003a0jsn6f142ec40a21",
+        "X-RapidAPI-Host": "coingecko.p.rapidapi.com",
+      },
+    };
+
+    axios
+      .request(results)
+      .then((res) => {
+        setData(res.data.data);
+        console.log(res.data.data);
+      })
+      .catch(function (err) {
+        console.error(err);
+      });
+  }, []);
+  return {
+    data,
+  };
+};
+
+export default useFetchMktData;
--- src/components/RightSide.jsx
+++ src/components/RightSide.jsx
@@ -11,43 +11,21 @@
  * - Modification    :
  **/
 
-import axios from "axios";
 import React, { useEffect, useState } from "react";
 import DisplayBanner from "./DisplayBanner";
 import News from "./News";
+import useFetchMktData from "../../hooks/useFetchMktData";
 
 const RightHandSide = () => {
-  // fetch data for banner
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    const results = {
-      method: "GET",
-      url: "https://coingecko.p.rapidapi.com/global",
-      headers: {
-        "X-RapidAPI-Key": "e23fcb3be8msh1c0ed783cf62ab4p1003a0jsn6f142ec40a21",
-        "X-RapidAPI-Host": "coingecko.p.rapidapi.com",
-      },
-    };
-
-    axios
-      .request(results)
-      .then((res) => {
-        setData(res.data.data);
-        console.log(res.data.data);
-      })
-      .catch(function (err) {
-        console.error(err);
-      });
-  }, []);
+  const { data } = useFetchMktData();
   return (
     <>
       {/* map data for banner */}
-        <DisplayBanner
-          active={data.active_cryptocurrencies}
-          ongoing={data.ongoing_icos}
-          markets={data.markets}
-        />
+      <DisplayBanner
+        active={data.active_cryptocurrencies}
+        ongoing={data.ongoing_icos}
+        markets={data.markets}
+      />
       <News />
     </>
   );
