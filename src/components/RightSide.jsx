a2ef76cb5e64038bb88a35c0a21dd8bb273b1ea8
7heMagician <obbengvincent@gmail.com>
2022-08-24 20:44:16 +0200

    cleaned up

--- src/components/RightSide.jsx
+++ src/components/RightSide.jsx
@@ -11,44 +11,26 @@
  * - Modification    :
  **/
 
-<<<<<<< HEAD
 import React from "react";
 import DisplayBanner from "./DisplayBanner";
 import News from "./News";
-import useFetchMktData from "../../hooks/useFetchMktData";
 import useFetchNews from "../../hooks/useFetchNews";
+import DataLoader from "./DataLoader";
 
 const RightHandSide = () => {
-  const { data } = useFetchMktData();
   const { news } = useFetchNews();
   return (
     <>
       {/* map data for banner */}
-      <DisplayBanner
-        active={data.active_cryptocurrencies}
-        ongoing={data.ongoing_icos}
-        markets={data.markets}
-      />
+      <DataLoader>
+        <DisplayBanner />
+      </DataLoader>
       {/* map data for news */}
       {news.map((item, i) => (
         <News key={i} title={item.title} url={item.url} source={item.source} />
       ))}
-=======
-import React, { useEffect, useState } from "react";
-import DataLoader from "./DataLoader";
-import DisplayBanner from "./DisplayBanner";
-import News from "./News";
-
-const RightHandSide = () => {
-  return (
-    <>
-      <DataLoader>
-        <DisplayBanner />
-      </DataLoader>
-      <News />
->>>>>>> 9b9926d81551a4489c6cf2f52acee8a0451a8ae2
     </>
-  );
+  )
 };
 
 export default RightHandSide;
